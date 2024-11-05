63aa3caa1e65d7e3fab8eb1539939012e2b762b9
Suwan Aryal <aryalsuwan.2003@gmail.com>
2024-11-05 01:00:21 -0600

    Frontend design changes

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Password Manager.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Password Manager.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Password Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cbd3bb03ecc1998fca381b59e7b3727cad1cc6ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fa5345260def813a849ee535b41db73cc32a2bce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Password Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Password Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
